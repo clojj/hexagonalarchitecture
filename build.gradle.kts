5e97c9b50bf2b529b13370a586ac6f9ebfcf2343
clojj <clojj@github.com>
2022-09-03 23:12:14 +0200

    fix group

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     kotlin("plugin.spring") version "1.7.10"
 }
 
-group = "{{ group_name }}"
+group = "com.github.clojj"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
