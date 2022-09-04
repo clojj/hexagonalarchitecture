76b4dfa4b86afca184eec08ed44bc325ccfa8ace
clojj <clojj@github.com>
2022-09-04 15:32:32 +0200

    reformat & add archtest inbound

--- src/test/kotlin/com/github/hexagonalarchitecture/architecturetest/HexagonalArchitectureTest.kt
+++ src/test/kotlin/com/github/hexagonalarchitecture/architecturetest/HexagonalArchitectureTest.kt
@@ -8,59 +8,79 @@ import org.junit.jupiter.api.Test
 class HexagonalArchitectureTest {
 
     private val importedClasses = ClassFileImporter()
-        .withImportOption(DO_NOT_INCLUDE_TESTS)
-        .importPackages("com.github.hexagonalarchitecture")
+            .withImportOption(DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.github.hexagonalarchitecture")
 
     @Test
     fun `domain should depend on domain only`() {
         classes()
-            .that()
-            .resideInAPackage("..domain..")
-            .should()
-            .onlyDependOnClassesThat()
-            .resideInAnyPackage(
-                "..domain..",
-                "java..",
-                "org.jetbrains..",
-                "kotlin..",
-                "arrow.."
-            )
-            .check(importedClasses)
+                .that()
+                .resideInAPackage("..domain..")
+                .should()
+                .onlyDependOnClassesThat()
+                .resideInAnyPackage(
+                        "..domain..",
+                        "java..",
+                        "org.jetbrains..",
+                        "kotlin..",
+                        "arrow.."
+                )
+                .check(importedClasses)
     }
 
     @Test
-    fun `application services should not depend on infrastructure`() {
+    fun `application should not depend on infrastructure`() {
         classes()
-            .that()
-            .resideInAPackage("..application..")
-            .should()
-            .onlyDependOnClassesThat()
-            .resideInAnyPackage(
-                "..domain..",
-                "..application..",
-                "java..",
-                "org.jetbrains..",
-                "kotlin..",
-                "arrow.."
-            )
-            .check(importedClasses)
+                .that()
+                .resideInAPackage("..application..")
+                .should()
+                .onlyDependOnClassesThat()
+                .resideInAnyPackage(
+                        "..domain..",
+                        "..application..",
+                        "java..",
+                        "org.jetbrains..",
+                        "kotlin..",
+                        "arrow.."
+                )
+                .check(importedClasses)
     }
 
     @Test
-    fun `outbound should not depend on inbound nor application services`() {
+    fun `outbound should not depend on inbound & application`() {
         classes()
-            .that()
-            .resideInAPackage("..outbound..")
-            .should()
-            .onlyDependOnClassesThat()
-            .resideInAnyPackage(
-                "..outbound..",
-                "..domain..",
-                "java..",
-                "org.springframework..",
-                "org.jetbrains..",
-                "kotlin.."
-            )
-            .check(importedClasses)
+                .that()
+                .resideInAPackage("..outbound..")
+                .should()
+                .onlyDependOnClassesThat()
+                .resideInAnyPackage(
+                        "..outbound..",
+                        "..domain..",
+                        "java..",
+                        "org.springframework..",
+                        "org.jetbrains..",
+                        "kotlin.."
+                )
+                .check(importedClasses)
+    }
+
+    @Test
+    fun `inbound should not depend on outbound`() {
+        classes()
+                .that()
+                .resideInAPackage("..inbound..")
+                .should()
+                .onlyDependOnClassesThat()
+                .resideInAnyPackage(
+                        "..inbound..",
+                        "..domain..",
+                        "..application..",
+                        "java..",
+                        "org.springframework..",
+                        "org.jetbrains..",
+                        "kotlin..",
+                        "arrow.."
+                )
+                .check(importedClasses)
     }
 }
